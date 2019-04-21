e7665e1e591ee85d576e761e1f908fd0fc645cef
Oleh Vaskiv <olegvaskiw@ukr.net>
2019-04-21 19:14:01 +0300

    Updates

---
+++ precache-manifest.63429a4a8d5000566b0ae1f35a734e90.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b712793c6c6686482845",
+    "url": "/MaterialApp/static/css/main.c9c90893.chunk.css"
+  },
+  {
+    "revision": "b712793c6c6686482845",
+    "url": "/MaterialApp/static/js/main.1da71bde.chunk.js"
+  },
+  {
+    "revision": "a30ecbf40efa83ce1cb6",
+    "url": "/MaterialApp/static/js/runtime~main.b8bb6ce9.js"
+  },
+  {
+    "revision": "39c8ed59952286030792",
+    "url": "/MaterialApp/static/js/2.9c79e4e1.chunk.js"
+  },
+  {
+    "revision": "6019826e6dc5f94b52410a7e1020662d",
+    "url": "/MaterialApp/index.html"
+  }
+];
